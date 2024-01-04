c509fb06e4bc81a1349a0c8294a78da6290d590b
antoniomiguel-77 <albino77miguel@gmail.com>
2024-01-04 09:57:18 +0100

    Alteração do consumo de api do factplus, do curl para o http client

--- app/Api/FactPlus.php
+++ app/Api/FactPlus.php
@@ -3,6 +3,7 @@
 namespace App\Api;
 
 use App\Models\DetailOrder;
+use Illuminate\Support\Facades\Http;
 
 class FactPlus{
     public static function create($orderid)
@@ -47,33 +48,33 @@ class FactPlus{
 
             //Chamada a API do Factplus
 
-            $col = json_encode($insert);
+            $response = Http::post('https://api.factplus.co.ao', [
+                'apicall' => 'CREATE',
+                'apikey' => '65899c23c9b6e95943468c44c9ecd952',
+                'document'=>[
+                    'type'=>'factura',
+                    'date'=>$date,
+                    'duedate'=>$duedate,
+                    'vref'=>$vref,
+                    'serie'=>$serie,
+                    'currency'=>'AOA',
+                    'exchange_rate'=>'0',
+                    'observation'=>'FActura de Pagamento',
+                    'retention'=>'',
+                ],
+                'client'=>[
+                    'name'=>'CONSUMIDOR FINAL',
+                    'nif'=>'99999999',
+                    'email'=>'',
+                    'city'=>'Luanda',
+                    'address'=>'Luanda-Angola',
+                    'postalcode'=>'',
+                    'country'=>'Angola',
+                ],
+                'items'=>$insert
+            ]);
 
-
-            $curl = curl_init();
-            curl_setopt_array($curl, array(
-            CURLOPT_URL => "https://api.factplus.co.ao",
-            CURLOPT_RETURNTRANSFER => true,
-            CURLOPT_ENCODING => "",
-            CURLOPT_MAXREDIRS => 10,
-            CURLOPT_TIMEOUT => 300,
-            CURLOPT_FOLLOWLOCATION => true,
-            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-            CURLOPT_CUSTOMREQUEST => "POST",
-            CURLOPT_POSTFIELDS =>"{\r\n      \"apicall\":\"CREATE\",\r\n      \"apikey\": \"65899c23c9b6e95943468c44c9ecd952\",\r\n      \"document\": {\r\n        \"type\": \"factura\",\r\n        \"date\": \"$date\",\r\n        \"duedate\": \"$duedate\",\r\n        \"vref\": \"$vref\",\r\n        \"serie\":\"$serie\",\r\n        \"currency\":\"AOA\",\r\n        \"exchange_rate\":\"0\",\r\n        \"observation\":\"Factura de Pagamento\",\r\n        \"retention\":\"\"\r\n        },\r\n      \"client\":{\r\n        \"name\": \"Consumidor Final\",\r\n        \"nif\": \"999999999\",\r\n        \"email\": \"consumidor@gmail.com\",\r\n        \"city\": \"Luanda\",\r\n        \"address\":\"Luanda,Angola\",\r\n        \"postalcode\":\"\",\r\n        \"country\":\"Angola\"\r\n      },\r\n       \"items\": $col\r\n    }",
-            CURLOPT_HTTPHEADER => array(
-              "Content-Type: application/json"
-            ),
-          ));
-      
-          $response = curl_exec($curl);
-      
-          curl_close($curl);
-      
-      
-          $collection = collect(json_decode($response,true));
-
-          return $collection['data'];
+          return $response['data'];
            
     
          
@@ -120,30 +121,22 @@ class FactPlus{
     public static function sendInvoice($reference,$email)
     {
         try {
-            $curl = curl_init();
-            curl_setopt_array($curl, array(
-              CURLOPT_URL => "https://api.factplus.co.ao",
-              CURLOPT_RETURNTRANSFER => true,
-              CURLOPT_ENCODING => "",
-              CURLOPT_MAXREDIRS => 10,
-              CURLOPT_TIMEOUT => 0,
-              CURLOPT_FOLLOWLOCATION => true,
-              CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-              CURLOPT_CUSTOMREQUEST => "POST",
-              CURLOPT_POSTFIELDS =>"{\r\n      \"apicall\":\"SEND\",\r\n      \"apikey\": \"65899c23c9b6e95943468c44c9ecd952\",\r\n      \"document\": {\r\n        \"reference\": \"$reference\",\r\n        \"type\": \"factura\"\r\n  }, \"recipient\": {\r\n        \"address\": \"$email\",\r\n        \"subject\": \"Pagamento da Factura\",\r\n \"message\": \"caríssimo(s), Em anexo a sua factura ( RESTAURANTE KARAMBA)\",\r\n \"copy\": \"0\"\r\n  }   }",
-              CURLOPT_HTTPHEADER => array(
-                "Content-Type: application/json"
-              ),
-            ));
-          
-            $response = curl_exec($curl);
-            
-            curl_close($curl);
-            
-            
-            $collection = json_decode($response,true);
+            $response = Http::post('https://api.factplus.co.ao', [
+                'apicall' => 'SEND',
+                'apikey' => '65899c23c9b6e95943468c44c9ecd952',
+                'document'=>[
+                    'reference'=>$reference,
+                    'type'=>'factura',
+                ],
+                'recipient'=>[
+                    'address'=>$email,
+                    'subject'=>'FACTURA DE PAGAMENTO',
+                    'message'=>'SUA FACTURA DE PAGAMENTO, RESTAURANTE KARAMBA',
+                    'copy'=>'0',
+                ]
+            ]);
 
-          return $collection['data'];
+          return $response['data'];
         } catch (\Throwable $th) {
             return redirect()->back()->with('error','Falha ao realizar Operação');
         }
@@ -151,30 +144,18 @@ class FactPlus{
     public static function changeStatu($reference)
     {
         try {
-                $curl = curl_init();
-                curl_setopt_array($curl, array(
-                CURLOPT_URL => "https://api.factplus.co.ao",
-                CURLOPT_RETURNTRANSFER => true,
-                CURLOPT_ENCODING => "",
-                CURLOPT_MAXREDIRS => 10,
-                CURLOPT_TIMEOUT => 0,
-                CURLOPT_FOLLOWLOCATION => true,
-                CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-                CURLOPT_CUSTOMREQUEST => "POST",
-                CURLOPT_POSTFIELDS =>"{\r\n      \"apicall\":\"ALTER\",\r\n      \"apikey\": \"65899c23c9b6e95943468c44c9ecd952\",\r\n      \"document\": {\r\n        \"reference\": \"$reference\",\r\n        \"type\": \"factura\",\r\n \"status\": \"settled\",\r\n \"reason\": \"\"\r\n   }   }",
-                CURLOPT_HTTPHEADER => array(
-                "Content-Type: application/json"
-                ),
-                ));
+            $response = Http::post('https://api.factplus.co.ao', [
+                'apicall' => 'ALTER',
+                'apikey' => '65899c23c9b6e95943468c44c9ecd952',
+                'document'=>[
+                    'type'=>'factura',
+                    'reference'=>'1704357099',
+                    'status'=>'sent',
+                    'reason'=>'',
+                ]
+            ]);
 
-                $response = curl_exec($curl);
-
-                curl_close($curl);
-
-
-                $collection = json_decode($response,true);
-
-          return $collection['data'];
+          return $response['data'];
         } catch (\Throwable $th) {
             return redirect()->back()->with('error','Falha ao realizar Operação');
         }
--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,93 @@
 <?php
 
 use App\Events\ChannelPublic;
+use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Route;
 
  
+
+
+// Route::get('/send',function(){
+
+//     $response = Http::post('https://api.factplus.co.ao', [
+//         'apicall' => 'SEND',
+//         'apikey' => '65899c23c9b6e95943468c44c9ecd952',
+//         'document'=>[
+//             'reference'=>'1704294369',
+//             'type'=>'factura',
+//         ],
+//         'recipient'=>[
+//             'address'=>'pachecobarrosodig4@gmail.com',
+//             'subject'=>'FACTURA DE PAGAMENTO',
+//             'message'=>'SUA FACTURA DE PAGAMENTO, RESTAURANTE KARAMBA',
+//             'copy'=>'0',
+//         ]
+//     ]);
+
+
+//     dd($response['data']);
+// });
+// Route::get('/alter',function(){
+
+//     $response = Http::post('https://api.factplus.co.ao', [
+//         'apicall' => 'ALTER',
+//         'apikey' => '65899c23c9b6e95943468c44c9ecd952',
+//         'document'=>[
+//             'type'=>'factura',
+//             'reference'=>'1704357099',
+//             'status'=>'sent',
+//             'reason'=>'',
+//         ]
+//     ]);
+
+
+//     dd($response['result']);
+// });
+// Route::get('/create',function(){
+
+
+    
+
+//     $response = Http::post('https://api.factplus.co.ao', [
+//         'apicall' => 'CREATE',
+//         'apikey' => '65899c23c9b6e95943468c44c9ecd952',
+//         'document'=>[
+//             'type'=>'factura',
+//             'date'=>'2024-01-04',
+//             'duedate'=>'2024-01-10',
+//             'vref'=>'settled',
+//             'serie'=>'2024',
+//             'currency'=>'AOA',
+//             'exchange_rate'=>'0',
+//             'observation'=>'FActura de Pagamento',
+//             'retention'=>'',
+//         ],
+//         'client'=>[
+//             'name'=>'CONSUMIDOR FINAL',
+//             'nif'=>'99999999',
+//             'email'=>'test@gmail.com',
+//             'city'=>'Luanda',
+//             'address'=>'Luanda-Angola',
+//             'postalcode'=>'',
+//             'country'=>'Angola',
+//         ],
+//         'items'=>[
+//             [
+//             'itemcode'=>'WEB001',
+//             'description'=>'Website',
+//             'price'=>'200000',
+//             'quantity'=>'2',
+//             'tax'=>'0%',
+//             'discount'=>'0',
+//             'exemption_code'=>'M11',
+//             'retention'=>'',
+//             ]
+//         ]
+//     ]);
+
+
+//     dd($response['message']);
+// });
 
 require __DIR__ .'/admin/routes.php';
 require __DIR__ .'/chef/routes.php';

b5fbb16bce692c2a5021be0b0ed895ac2d7300d6
antoniomiguel-77 <albino77miguel@gmail.com>
2024-01-06 17:30:56 +0100

    Descomentado as keys da api da conta legal

--- app/Api/FactPlus.php
+++ app/Api/FactPlus.php
@@ -18,9 +18,9 @@ class FactPlus {
     {
         DB::beginTransaction();
         //real
-        //$key = '65847d93edbb6d77bea624101ff616ea';
+        $key = '65847d93edbb6d77bea624101ff616ea';
         //teste
-        $key = '65995993b16b93cdac74e28f1cd69267';
+        //$key = '65995993b16b93cdac74e28f1cd69267';
         try {
 
            $details =  DetailOrder::where('order_id','=',$orderid)
@@ -105,9 +105,9 @@ class FactPlus {
     {
         DB::beginTransaction();
         //real
-        //$key = '65847d93edbb6d77bea624101ff616ea';
+        $key = '65847d93edbb6d77bea624101ff616ea';
         //teste
-        $key = '65995993b16b93cdac74e28f1cd69267';
+        //$key = '65995993b16b93cdac74e28f1cd69267';
         try {
             $response = Http::post('https://api.factplus.co.ao', [
                 'apicall' => 'SEND',
@@ -137,9 +137,9 @@ class FactPlus {
     {
         DB::beginTransaction();
          //real
-        //$key = '65847d93edbb6d77bea624101ff616ea';
+        $key = '65847d93edbb6d77bea624101ff616ea';
         //teste
-        $key = '65995993b16b93cdac74e28f1cd69267';
+        //$key = '65995993b16b93cdac74e28f1cd69267';
         try {
             $response = Http::post('https://api.factplus.co.ao', [
                 'apicall' => 'ALTER',

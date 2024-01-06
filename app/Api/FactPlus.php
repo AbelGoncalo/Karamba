a807cbdbb0d5ca3817554405ce6f5bca17f9e123
antoniomiguel-77 <albino77miguel@gmail.com>
2024-01-06 19:44:15 +0100

    add

--- app/Api/FactPlus.php
+++ app/Api/FactPlus.php
@@ -18,9 +18,9 @@ class FactPlus {
     {
         DB::beginTransaction();
         //real
-        //$key = '65847d93edbb6d77bea624101ff616ea';
+        $key = '65847d93edbb6d77bea624101ff616ea';
         //teste
-        $key = '65995993b16b93cdac74e28f1cd69267';
+        //$key = '65995993b16b93cdac74e28f1cd69267';
         try {
 
            $details =  DetailOrder::where('order_id','=',$orderid)
@@ -63,35 +63,35 @@ class FactPlus {
 
             //Chamada a API do Factplus
 
-            //Chamada a API do Factplus
+            $response = Http::post('https://api.factplus.co.ao', [
+                'apicall' => 'CREATE',
+                'apikey' => $key,
+                'document'=>[
+                    'type'=>'factura',
+                    'date'=>$date,
+                    'duedate'=>$duedate,
+                    'vref'=>$vref,
+                    'serie'=>$serie,
+                    'currency'=>'AOA',
+                    'exchange_rate'=>'0',
+                    'observation'=>'FActura de Pagamento',
+                    'retention'=>'',
+                ],
+                'client'=>[
+                    'name'=>'CONSUMIDOR FINAL',
+                    'nif'=>'99999999',
+                    'email'=>'',
+                    'city'=>'Luanda',
+                    'address'=>'Luanda-Angola',
+                    'postalcode'=>'',
+                    'country'=>'Angola',
+                ],
+                'items'=>$insert
+            ])->json();
 
-            $col = json_encode($insert);
-
-
-            $curl = curl_init();
-          curl_setopt_array($curl, array(
-            CURLOPT_URL => "https://api.factplus.co.ao",
-            CURLOPT_RETURNTRANSFER => true,
-            CURLOPT_ENCODING => "",
-            CURLOPT_MAXREDIRS => 10,
-            CURLOPT_TIMEOUT => 0,
-            CURLOPT_FOLLOWLOCATION => true,
-            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-            CURLOPT_CUSTOMREQUEST => "POST",
-            CURLOPT_POSTFIELDS =>"{\r\n      \"apicall\":\"CREATE\",\r\n      \"apikey\": \"$key\",\r\n      \"document\": {\r\n        \"type\": \"factura\",\r\n        \"date\": \"$date\",\r\n        \"duedate\": \"$duedate\",\r\n        \"vref\": \"$vref\",\r\n        \"serie\":\"$serie\",\r\n        \"currency\":\"AOA\",\r\n        \"exchange_rate\":\"0\",\r\n        \"observation\":\"Factura de Pagamento\",\r\n        \"retention\":\"\"\r\n        },\r\n      \"client\":{\r\n        \"name\": \"Consumidor Final\",\r\n        \"nif\": \"999999999\",\r\n        \"email\": \"consumidor@gmail.com\",\r\n        \"city\": \"Luanda\",\r\n        \"address\":\"Luanda,Angola\",\r\n        \"postalcode\":\"\",\r\n        \"country\":\"Angola\"\r\n      },\r\n       \"items\": $col\r\n    }",
-            CURLOPT_HTTPHEADER => array(
-              "Content-Type: application/json"
-            ),
-          ));
-      
-          $response = curl_exec($curl);
-      
-          curl_close($curl);
-      
-      
-          $collection = collect(json_decode($response,true));
-
-          return $collection['data'];
+    
+            $collection = collect(json_decode($response,true));
+            return $collection;
            
     
           DB::commit();

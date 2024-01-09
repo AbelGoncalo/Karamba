cfdccbfe99e2c2e03ada54cd3a67c0eaf96506a9
antoniomiguel-77 <albino77miguel@gmail.com>
2024-01-09 14:37:00 +0100

    Alterado o tempo limite das requisições

--- app/Api/FactPlus.php
+++ app/Api/FactPlus.php
@@ -80,7 +80,7 @@ class FactPlus {
          CURLOPT_RETURNTRANSFER => true,
          CURLOPT_ENCODING => "",
          CURLOPT_MAXREDIRS => 10,
-         CURLOPT_TIMEOUT => 460,
+         CURLOPT_TIMEOUT => 300,
          CURLOPT_FOLLOWLOCATION => true,
          CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
          CURLOPT_CUSTOMREQUEST => "POST",

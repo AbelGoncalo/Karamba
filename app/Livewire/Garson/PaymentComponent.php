d76d3b802b730a3b15ddb7e868e27fccd104639c
antoniomiguel-77 <albino77miguel@gmail.com>
2024-01-09 17:40:19 +0100

    comentado outros metodos da api

--- app/Livewire/Garson/PaymentComponent.php
+++ app/Livewire/Garson/PaymentComponent.php
@@ -302,7 +302,7 @@ class PaymentComponent extends Component
 
         $reference =    \App\Api\FactPlus::create($order->id,$this->name,$this->nif,$this->address);
         dd($reference);
-        \App\Api\FactPlus::changeStatu($reference,'sent');
+        //\App\Api\FactPlus::changeStatu($reference,'sent');
 
             session()->put('finallyOrder',$reference);
             session()->put('table',$this->tableNumber);
@@ -355,7 +355,7 @@ class PaymentComponent extends Component
               
              
               $this->clearFields();
-             \App\Api\FactPlus::sendInvoice(session('finallyOrder'),$this->email);
+             //\App\Api\FactPlus::sendInvoice(session('finallyOrder'),$this->email);
               session()->forget('finallyOrder');
               session()->forget('table');
 
